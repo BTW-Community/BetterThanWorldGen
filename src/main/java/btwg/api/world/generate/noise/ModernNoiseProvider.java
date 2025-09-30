674aedabb6c96eecd9a453e8dabc3ec56493a2b6
Abigail Moore <abigailmmoore00@gmail.com>
2025-09-29 23:02:51 -0600

    Tweaked erosion parameters

--- src/main/java/btwg/api/world/generate/noise/ModernNoiseProvider.java
+++ src/main/java/btwg/api/world/generate/noise/ModernNoiseProvider.java
@@ -10,18 +10,18 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ModernNoiseProvider extends NoiseProvider {
-    public static final int CONTINENTALNESS_SCALE = 4096;
-    public static final int EROSION_SCALE = 2048;
-    public static final int RIDGES_SCALE = 384;
+    public static final int CONTINENTALNESS_SCALE = 2048;
+    public static final int EROSION_SCALE = 512;
+    public static final int RIDGES_SCALE = 256;
     public static final int WEIRDNESS_SCALE = 256;
 
     public static final int TEMPERATURE_SCALE = 1024;
     public static final int HUMIDITY_SCALE = 512;
 
-    public static final int TERRAIN_SCALE = 384;
+    public static final int TERRAIN_SCALE = 256;
 
     private static final int DRIVER_OCTAVES = 4;
-    private static final int TERRAIN_OCTAVES = 6;
+    private static final int TERRAIN_OCTAVES = 5;
 
     // TODO: fix interpolation
     public static final int NOISE_SUBSCALE = 16;
@@ -40,27 +40,27 @@ public class ModernNoiseProvider extends NoiseProvider {
     );
 
     private static final Spline erosionToThickness = Spline.of(
-            new Key(-1.0, 8),  // low erosion: sharp
-            new Key(-0.5, 10),
-            new Key( 0.0, 12),
-            new Key( 0.5, 14),
-            new Key( 1.0, 16)   // high erosion: soft
+            new Key(-1.0, 0.2),
+            new Key(-0.5, 0.4),
+            new Key( 0.0, 0.6),
+            new Key( 0.5, 0.8),
+            new Key( 1.0, 1.0)
     );
 
     private static final Spline erosionToAmplitude = Spline.of(
-            new Key(-1.0, 10),
-            new Key(-0.5, 8),
-            new Key( 0.0,  6),
-            new Key( 0.5,  4),
-            new Key( 1.0,  3)
+            new Key(-1.0, 1.0),
+            new Key(-0.5, 0.8),
+            new Key( 0.0,  0.6),
+            new Key( 0.5,  0.4),
+            new Key( 1.0,  0.3)
     );
 
     private static final Spline erosionToHeightBias = Spline.of(
-            new Key(-1.0,  16),
-            new Key(-0.5,  8),
-            new Key( 0.0,  4),
-            new Key( 0.5,  2),
-            new Key( 1.0,  1)
+            new Key(-1.0,  1.0),
+            new Key(-0.5,  0.5),
+            new Key( 0.0,  0.25),
+            new Key( 0.5,  0.125),
+            new Key( 1.0,  0.0625)
     );
 
     private final OpenSimplexOctavesFast continentalnessGenerator;
@@ -128,9 +128,13 @@ public class ModernNoiseProvider extends NoiseProvider {
             for (int k = 0; k < 16; k++) {
                 int colIdx = idx(i, k, 16);
 
-                double thickness = this.thickness[colIdx];
-                double amplitude = this.amplitude[colIdx];
-                double heightBias = this.heightBias[colIdx];
+                double baseThickness = 8;
+                double baseAmplitude = 10;
+                double baseHeightBias = 8;
+
+                double thickness = this.thickness[colIdx] * baseThickness;
+                double amplitude = this.amplitude[colIdx] * baseAmplitude;
+                double heightBias = this.heightBias[colIdx] * baseHeightBias;
 
                 double continentalness = this.continentalness[colIdx];
                 double height = continentalness * TOTAL_Y_HEIGHT + heightBias;
