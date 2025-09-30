a1012061ad328fea3d1a8bc9d5d14bef381761e1
Abigail Moore <abigailmmoore00@gmail.com>
2025-09-30 12:53:24 -0600

    Fixed microbiomes

--- src/main/java/btwg/api/biome/BiomeNoiseVector.java
+++ src/main/java/btwg/api/biome/BiomeNoiseVector.java
@@ -6,4 +6,12 @@ public record BiomeNoiseVector(
         double continentalness,
         double erosion,
         double weirdness
-) {}
+) {
+    public BiomeNoiseVector {
+        temperature = Math.round(temperature / 0.05) * 0.05;
+        humidity = Math.round(humidity / 0.05) * 0.05;
+        continentalness = Math.round(continentalness / 0.05) * 0.05;
+        erosion = Math.round(erosion / 0.05) * 0.05;
+        weirdness = Math.round(weirdness / 0.05) * 0.05;
+    }
+}
