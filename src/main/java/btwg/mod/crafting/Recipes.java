c56a49bb3d2eb783d14f277b338d32ca905bf232
Abigail Moore <abigailmmoore00@gmail.com>
2025-10-08 18:31:53 -0600

    Added stone recipes

--- src/main/java/btwg/mod/crafting/Recipes.java
+++ src/main/java/btwg/mod/crafting/Recipes.java
@@ -1,6 +1,7 @@
 package btwg.mod.crafting;
 
 import btw.block.BTWBlocks;
+import btw.block.blocks.AestheticOpaqueEarthBlock;
 import btw.block.blocks.MouldingAndDecorativeBlock;
 import btw.block.blocks.SidingAndCornerAndDecorativeBlock;
 import btw.crafting.recipe.RecipeManager;
@@ -22,6 +23,7 @@ public class Recipes {
         initTorchRecipes();
         initSoilRecipes();
         initWoodRecipes();
+        initStoneRecipes();
     }
 
     private static void initTorchRecipes() {
@@ -341,7 +343,314 @@ public class Recipes {
                 .forEach(Recipes::initStoneRecipesForType);
     }
 
-    private static void initStoneRecipesForType(StoneType stoneType) {
+    private static void initStoneRecipesForType(StoneType type) {
 
+        //------ Smooth Stone ------//
+
+        RecipeManager.addRecipe(new ItemStack(type.stoneID(), 1, type.stoneMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.stoneSlabID(), 1, type.stoneSlabMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.stoneSlabID(), 6, type.stoneSlabMetadata()), new Object[] {
+                "###",
+                '#', new ItemStack(type.stoneID(), 1, type.stoneMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.stoneStairsID(), 6, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.stoneID(), 1, type.stoneMetadata())
+        });
+
+        //------ Loose Cobblestone ------//
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata()), new Object[] {
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.looseCobblestoneSlabID(), 1, type.looseCobblestoneSlabMetadata()), new Object[] {
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+                new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseCobblestoneStairsID(), 1, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.rockItemID(), 1, type.rockItemMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.rockItemID(), 8, type.rockItemMetadata()), new Object[] {
+                new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata())
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.rockItemID(), 4, type.rockItemMetadata()), new Object[] {
+                new ItemStack(type.looseCobblestoneSlabID(), 1, type.looseCobblestoneSlabMetadata())
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.rockItemID(), 6, type.rockItemMetadata()), new Object[] {
+                new ItemStack(type.looseCobblestoneStairsID(), 1, 0)
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.rockItemID(), 2, type.rockItemMetadata()), new Object[] {
+                BTWTags.chisels,
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseCobblestoneSlabID(), 4, type.looseCobblestoneSlabMetadata()), new Object[] {
+                "##",
+                '#', new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseCobblestoneStairsID(), 4, 0), new Object[] {
+                "# ",
+                "##",
+                '#', new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseCobblestoneStairsID(), 8, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.looseCobblestoneSlabID(), 1, type.looseCobblestoneSlabMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseCobblestoneStairsID(), 2, 0), new Object[] {
+                "# ",
+                "##",
+                '#', new ItemStack(type.looseCobblestoneSlabID(), 1, type.looseCobblestoneSlabMetadata())
+        });
+
+        RecipeManager.addPistonPackingRecipe(Block.blocksList[type.looseCobblestoneID()], type.looseCobblestoneMetadata(),
+                new ItemStack(type.rockItemID(), 8, type.rockItemMetadata())
+        );
+
+        //------ Cobblestone ------//
+
+        RecipeManager.addRecipe(new ItemStack(type.cobblestoneID(), 1, type.cobblestoneMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.cobblestoneSlabID(), 1, type.cobblestoneSlabMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.cobblestoneSlabID(), 6, type.cobblestoneSlabMetadata()), new Object[] {
+                "###",
+                '#', new ItemStack(type.cobblestoneID(), 1, type.cobblestoneMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.cobblestoneStairsID(), 6, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.cobblestoneID(), 1, type.cobblestoneMetadata())
+        });
+
+        //------ Loose Stone Bricks ------//
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.looseCobblestoneID(), 1, type.looseCobblestoneMetadata()), new Object[] {
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.looseStoneBrickSlabID(), 1, type.looseStoneBrickSlabMetadata()), new Object[] {
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+                new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickStairsID(), 1, 0), new Object[] {
+                "# ",
+                "##",
+                '#', new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickStairsID(), 2, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.stoneBrickItemID(), 1, type.stoneBrickItemMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.stoneBrickItemID(), 4, type.stoneBrickItemMetadata()), new Object[] {
+                new ItemStack(type.looseStoneBrickID(), 1, type.looseStoneBrickMetadata())
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.stoneBrickItemID(), 2, type.stoneBrickItemMetadata()), new Object[] {
+                new ItemStack(type.looseStoneBrickSlabID(), 1, type.looseStoneBrickSlabMetadata())
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.stoneBrickItemID(), 3, type.stoneBrickItemMetadata()), new Object[] {
+                new ItemStack(type.looseStoneBrickStairsID(), 1, 0)
+        });
+
+        RecipeManager.addShapelessRecipeWithSecondaryOutputIndicator(new ItemStack(type.stoneBrickItemID(), 4, type.stoneBrickItemMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new Object[] {
+                        BTWTags.chisels,
+                        new ItemStack(type.stoneID(), 1, type.stoneMetadata()),
+                });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickSlabID(), 4, type.looseStoneBrickSlabMetadata()), new Object[] {
+                "##",
+                '#', new ItemStack(type.looseStoneBrickID(), 1, type.looseStoneBrickMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickStairsID(), 4, 0), new Object[] {
+                "# ",
+                "##",
+                '#', new ItemStack(type.looseStoneBrickID(), 1, type.looseStoneBrickMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickStairsID(), 8, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.looseStoneBrickID(), 1, type.looseStoneBrickMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickID(), 1, type.looseStoneBrickMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.looseStoneBrickSlabID(), 1, type.looseStoneBrickSlabMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseStoneBrickStairsID(), 2, 0), new Object[] {
+                "# ",
+                "##",
+                '#', new ItemStack(type.looseStoneBrickSlabID(), 1, type.looseStoneBrickSlabMetadata())
+        });
+
+        RecipeManager.addPistonPackingRecipe(Block.blocksList[type.looseStoneBrickID()], type.looseStoneBrickMetadata(),
+                new ItemStack(type.stoneBrickItemID(), 4, type.stoneBrickItemMetadata())
+        );
+
+        //------ Stone Bricks ------//
+
+        RecipeManager.addRecipe(new ItemStack(type.stoneBrickID(), 1, type.stoneBrickMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.stoneBrickSlabID(), 1, type.stoneBrickSlabMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.stoneBrickSlabID(), 6, type.stoneBrickSlabMetadata()), new Object[] {
+                "###",
+                '#', new ItemStack(type.stoneBrickID(), 1, type.stoneBrickMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.stoneBrickStairsID(), 6, 0), new Object[] {
+                "#  ",
+                "## ",
+                "###",
+                '#', new ItemStack(type.stoneBrickID(), 1, type.stoneBrickMetadata())
+        });
+
+        RecipeManager.addSoulforgeRecipe(new ItemStack(type.chiseledStoneBrickID(), 12, type.chiseledStoneBrickMetadata()), new Object[] {
+                "####",
+                "#  #",
+                "#  #",
+                "####",
+                '#', new ItemStack(type.stoneBrickID(), 1, type.stoneBrickMetadata()),
+        });
+
+        //------ Regolith ------//
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.looseDirtID(), 1, type.looseDirtMetadata()), new Object[] {
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.looseDirtSlabID(), 1, type.looseDirtSlabMetadata()), new Object[] {
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+                new ItemStack(type.dirtPileID(), 1, type.dirtPileMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.dirtPileID(), 8, type.dirtPileMetadata()), new Object[] {
+                new ItemStack(type.looseDirtID(), 1, type.looseDirtMetadata())
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.dirtPileID(), 4, type.dirtPileMetadata()), new Object[] {
+                new ItemStack(type.looseDirtSlabID(), 1, type.looseDirtSlabMetadata())
+        });
+
+        RecipeManager.addPistonPackingRecipe(Block.blocksList[type.dirtID()], type.dirtMetadata(),
+                new ItemStack(type.dirtPileID(), 8, type.dirtPileMetadata())
+        );
+
+        RecipeManager.addRecipe(new ItemStack(type.looseDirtSlabID(), 4, type.looseDirtSlabMetadata()), new Object[] {
+                "##",
+                '#', new ItemStack(type.looseDirtID(), 1, type.looseDirtMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.looseDirtID(), 1, type.looseDirtMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.looseDirtSlabID(), 1, type.looseDirtSlabMetadata())
+        });
+
+        //------ Gravel ------//
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.gravelID(), 1, type.gravelMetadata()), new Object[] {
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.gravelSlabID(), 1, type.gravelSlabMetadata()), new Object[] {
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+                new ItemStack(type.gravelPileID(), 1, type.gravelPileMetadata()),
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.gravelPileID(), 8, type.gravelPileMetadata()), new Object[] {
+                new ItemStack(type.gravelID(), 1, type.gravelMetadata())
+        });
+
+        RecipeManager.addShapelessRecipe(new ItemStack(type.gravelPileID(), 4, type.gravelPileMetadata()), new Object[] {
+                new ItemStack(type.gravelSlabID(), 1, type.gravelSlabMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.gravelSlabID(), 4, type.gravelSlabMetadata()), new Object[] {
+                "##",
+                '#', new ItemStack(type.gravelID(), 1, type.gravelMetadata())
+        });
+
+        RecipeManager.addRecipe(new ItemStack(type.gravelID(), 1, type.gravelMetadata()), new Object[] {
+                "#",
+                "#",
+                '#', new ItemStack(type.gravelSlabID(), 1, type.gravelSlabMetadata())
+        });
     }
 }
